432f0be3ba01ca64378f81d3bc0775cd42186630
Christopher Beddies <crisio@Lisa.fritz.box>
2018-07-31 22:41:21 +0200

    Initial commit

---
+++ app/src/main/java/de/a_b_software/anime_on_demand_kaze/MainActivity.java
@@ -0,0 +1,13 @@
+package de.a_b_software.anime_on_demand_kaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
