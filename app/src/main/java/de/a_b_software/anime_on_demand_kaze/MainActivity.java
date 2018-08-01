6652a645c083dada6bda210c39c0196025b8fbe3
CrisioDev <crisioletsplay@gmail.com>
2018-08-01 20:57:41 +0200

    Login Cookie added

--- app/src/main/java/de/a_b_software/anime_on_demand_kaze/MainActivity.java
+++ app/src/main/java/de/a_b_software/anime_on_demand_kaze/MainActivity.java
@@ -3,6 +3,7 @@ package de.a_b_software.anime_on_demand_kaze;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -11,17 +12,18 @@ import android.widget.TextView;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "de.a-b-software.anime_on_demand_kaze.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +81,9 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 final StringBuilder builder = new StringBuilder();
                 try {
-                    Document aod = Jsoup.connect("https://www.anime-on-demand.de/").get();
+                    //Document aod = Jsoup.connect("https://www.anime-on-demand.de/").get();
+                    Connection.Response loginCon = Jsoup.connect("https://www.anime-on-demand.de/").method(Connection.Method.GET).execute();
+                    Document aod = loginCon.parse();
                     Elements forms = aod.getElementsByClass("form");
                     List<String> values = forms.first().getElementsByTag("input").eachAttr("value");
                     String action = forms.first().attr("action");
@@ -88,23 +92,31 @@ public class MainActivity extends AppCompatActivity {
                     String rememberme = values.get(2);
                     String remembermetoo = values.get(3);
                     String commit = values.get(4);
-                    System.out.println(token);
-                    builder.append(token);
+                    builder.append(utf8 + " ");
+                    builder.append(token + " ");
+                    builder.append(rememberme + " ");
+                    builder.append(user + " ");
+                    builder.append(password + " ");
+                    builder.append(commit + " ");
 
+                    System.out.println("UTF8 " + utf8);
+                    System.out.println("TOKEN " + token);
+                    System.out.println("USER " + user);
+                    System.out.println("PASSWORD " + password);
+                    System.out.println("COMMIT " + commit);
 
                     Document login = Jsoup.connect("https://www.anime-on-demand.de/users/sign_in")
                             .data("utf8",utf8)
                             .data("authenticity_token",token)
                             .data("user[login]",user)
                             .data("user[password]",password)
-                            .data("user[remember_me]",rememberme)
-                            .data("commit",commit)
+                            .data("user[remember_me]","0")
+                            .data("commit","Einloggen")
+                            .cookies(loginCon.cookies())
                             .followRedirects(true)
                             .post();
 
-                    Document aodloggedin = Jsoup.connect("https://www.anime-on-demand.de/").get();
-
-                    builder.append(aodloggedin.data());
+                    builder.append(login.text());
 
                 } catch (IOException e) {
                     e.printStackTrace();
